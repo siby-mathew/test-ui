eee5d52b76a0452cd0949a6944e99b8bac9fe6a5
Siby Mathew <siby@sonicdex.io>
2025-08-19 22:52:35 +0530

    dfx

--- src/hooks/useSolPrice.ts
+++ src/hooks/useSolPrice.ts
@@ -2,14 +2,19 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { QueryKeys } from "src/types";
 
-const fetchSOLPrice = async (): Promise<any> => {
-  const { data } = await axios.get<any>(
+type TokenPriceResponse<T extends string = "solana"> = {
+  [key in T]: {
+    usd: number;
+  };
+};
+
+const fetchSOLPrice = async (): Promise<number> => {
+  const { data } = await axios.get<TokenPriceResponse>(
     `${import.meta.env.VITE_SOLMAIL_PRICE_API}simple/price?ids=solana&vs_currencies=usd`
   );
   if (data && data.solana && data.solana.usd) {
     return data.solana.usd;
   }
-
   return 0;
 };
 
